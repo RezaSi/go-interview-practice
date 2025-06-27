f184e0133bfba96d2b2ef8fb78fa1702bf840fb1
mick4711 <mick4711@gmail.com>
2025-06-27 19:28:22 +0100

    save bin search TODO

--- challenge-21/solution-template.go
+++ challenge-21/solution-template.go
@@ -26,8 +26,68 @@ func main() {
 // BinarySearch performs a standard binary search to find the target in the sorted array.
 // Returns the index of the target if found, or -1 if not found.
 func BinarySearch(arr []int, target int) int {
-	// TODO: Implement this function
-	return -1
+	// TODO: Target in between test not detected -> inf loop
+	l := len(arr)
+	if l == 0 {
+		return -1
+	}
+	if l == 1 {
+		if arr[0] == target {
+			return 0
+		}
+		return -1
+	}
+
+	leftEnd, rightEnd := 0, l-1
+	midIdx := rightEnd / 2
+	// done := false
+
+	currLeft, currRight := leftEnd, rightEnd
+	for {
+		// see if we've hit the target, bingo
+		if arr[midIdx] == target {
+			return midIdx
+		}
+		// see if we've reached either end and we're done, not found
+		if midIdx == leftEnd || midIdx == rightEnd {
+			return -1
+		}
+		// go right
+		if arr[midIdx] < target {
+			currLeft = midIdx
+			midIdx = currLeft + (currRight-currLeft)/2
+			if midIdx == currLeft {
+				midIdx++
+			}
+		} else { // go left
+			currRight = midIdx
+			midIdx = currLeft + (currRight-currLeft)/2
+		}
+	}
+	/*
+			Start with the middle element of the array
+		If the target value equals the middle element, we're done
+		If the target value is less than the middle element, search the left half
+		If the target value is greater than the middle element, search the right half
+		Repeat until the element is found or the search space is empty
+		Binary search has a time complexity of O(log n), which is much more efficient than linear search (O(n)) for large datasets.
+
+		Implementation Approaches
+		Iterative Implementation:
+
+		Use a loop with left and right pointers
+		Calculate middle index in each iteration
+		Adjust pointers based on comparison with target
+		Continue until element is found or search space is exhausted
+
+	*/
+	// midIdx :=
+	// if l ==  {
+	// 	if arr[0] == target {
+	// 		return 0
+	// 	}
+	// }
+	return -2
 }
 
 // BinarySearchRecursive performs binary search using recursion.
