cc74290f7e77e2adcc3e5ef5d403b828af36edb2
odelbos <od@phibox.com>
2025-08-08 16:20:41 +0200

    fix #226 : Fiber Challenge 3 : Segmentation fault running the tests (validator not initialized in tests)

--- packages/fiber/challenge-3-validation-errors/solution-template.go
+++ packages/fiber/challenge-3-validation-errors/solution-template.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/go-playground/validator/v10"
 )
 
 // Product represents a product in the catalog
@@ -38,13 +39,14 @@ var products = []Product{
 }
 var nextID = 3
 
+var validate *validator.Validate
+
 func main() {
 	// TODO: Create Fiber app
 	app := fiber.New()
 
-	// TODO: Setup custom validator
-	// Use github.com/go-playground/validator/v10 package
-	// Register custom validators (SKU format)
+	// Setup custom validator
+	setupCustomValidator()
 
 	// TODO: Define routes
 	// GET /products - get all products with filtering
@@ -54,6 +56,12 @@ func main() {
 	// POST /products/bulk - create multiple products
 
 	// TODO: Start server on port 3000
+}
+
+func setupCustomValidator() {
+	// TODO: Setup custom validator
+	// Use github.com/go-playground/validator/v10 package
+	// Register custom validators (SKU format)
 }
 
 // TODO: Implement custom validators
