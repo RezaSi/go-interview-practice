58b13e63437c6f62ac0c9b03dccdc8e3237a1c18
kiramux <160688228+kiramux@users.noreply.github.com>
2025-11-08 09:20:42 +0800

    update ReverseSlice()

--- challenge-19/submissions/kiramux/solution-template.go
+++ challenge-19/submissions/kiramux/solution-template.go
@@ -61,13 +61,8 @@ func ReverseSlice(slice []int) []int {
 		return []int{}
 	}
 	result := make([]int, len(slice))
-	copy(result, slice)
-	left := 0
-	right := len(result) - 1
-	for left < right {
-		result[left], result[right] = result[right], result[left]
-		left++
-		right--
+	for i, v := range slice {
+		result[len(slice)-1-i] = v
 	}
 	return result
 }
