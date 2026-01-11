074ca029953d6bf37d16d15f548ab1dff8f18bf7
Jose Barahona <jrab66@users.noreply.github.com>
2026-01-11 14:54:13 -0600

    Update challenge-2/submissions/jrab66/solution-template.go
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- challenge-2/submissions/jrab66/solution-template.go
+++ challenge-2/submissions/jrab66/solution-template.go
@@ -23,11 +23,9 @@ func main() {
 
 // ReverseString returns the reversed string of s.
 func ReverseString(s string) string {
-	// TODO: Implement the function
-    var b strings.Builder
-	b.Grow(len(s))
-	for i := len(s) - 1; i >= 0; i-- {
-		b.WriteByte(s[i])
+	runes := []rune(s)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
 	}
-	return b.String()
+	return string(runes)
 }
