20c2378c9d38a9084fae021e29ce35666bd4f073
Mark Popov <hawk691@gmail.com>
2026-02-19 09:45:30 +0200

    Fix docstring and s.Spilt to s.Fields

--- challenge-6/submissions/PopovMarko/solution-template.go
+++ challenge-6/submissions/PopovMarko/solution-template.go
@@ -16,7 +16,6 @@ import (
 // For example:
 // Input: "The quick brown fox jumps over the lazy dog."
 // Output: map[string]int{"the": 2, "quick": 1, "brown": 1, "fox": 1, "jumps": 1, "over": 1, "lazy": 1, "dog": 1}
-// Func counts number of words in string
 func CountWordFrequency(text string) map[string]int {
 	res := make(map[string]int)
 	if text == "" {
@@ -24,10 +23,12 @@ func CountWordFrequency(text string) map[string]int {
 	}
 
 	lowerString := strings.ToLower(text)
+	lowerString = strings.ReplaceAll(lowerString, "'s", "s")
 	repString := strings.Map(func(r rune) rune {
 		if (r >= 'a' && r <= 'z') || (r >= '0' && r <= '9') {
 			return r
 		}
+
 		return ' '
 	}, lowerString)
 	for _, s := range strings.Fields(repString) {
