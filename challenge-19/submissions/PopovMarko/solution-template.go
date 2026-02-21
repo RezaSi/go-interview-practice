6415f931d20ed2ed2b681051c1d41ac185e60c82
Mark Popov <hawk691@gmail.com>
2026-02-21 10:04:44 +0200

    Fix: docstrings corrected, deleted one element check, loop in FindMax optimized

--- challenge-19/submissions/PopovMarko/solution-template.go
+++ challenge-19/submissions/PopovMarko/solution-template.go
@@ -28,7 +28,7 @@ func main() {
 // FindMax returns the maximum value in a slice of integers.
 // If the slice is empty, it returns 0.
 func FindMax(numbers []int) int {
-	// Check for emty slice
+	// Check for empty slice
 	if len(numbers) == 0 {
 		return 0
 	}
@@ -36,8 +36,8 @@ func FindMax(numbers []int) int {
 	// Variable for store the max value
 	max := numbers[0]
 
-	// Loop through the slice elemenes with max
-	for _, n := range numbers {
+	// Loop through the slice elements with max
+	for _, n := range numbers[1:] {
 		if n > max {
 			max = n
 		}
@@ -48,17 +48,17 @@ func FindMax(numbers []int) int {
 // RemoveDuplicates returns a new slice with duplicate values removed,
 // preserving the original order of elements.
 func RemoveDuplicates(numbers []int) []int {
-	// Check for emty slice
+	// Check for empty slice
 	if len(numbers) == 0 {
 		return []int{}
 	}
-	//New slice for store unic values
+	// New slice for store unique values
 	res := []int{}
 
-	// Map [int]bool to store wether elements seen in slice
+	// Map [int]bool to store whether elements seen in slice
 	duplicate := make(map[int]bool)
 
-	// Loop throght the numbers, check for duplicates and store in new slice
+	// Loop through the numbers, check for duplicates and store in new slice
 	for _, n := range numbers {
 		if !duplicate[n] {
 			duplicate[n] = true
@@ -70,21 +70,15 @@ func RemoveDuplicates(numbers []int) []int {
 
 // ReverseSlice returns a new slice with elements in reverse order.
 func ReverseSlice(slice []int) []int {
-	// Check for emty slice
+	// Check for empty slice
 	if len(slice) == 0 {
 		return []int{}
 	}
 
-	// New slice for store revesed elemenst
+	// New slice for store reversed elements
 	res := make([]int, 0, len(slice))
 
-	// Check for one element slice
-	if len(slice) == 1 {
-		res = append(res, slice[0])
-		return res
-	}
-
-	// Loop the slice in reverce order and write elemenes to new slice
+	// Loop the slice in reverse order and write elements to new slice
 	for i := len(slice) - 1; i >= 0; i-- {
 		res = append(res, slice[i])
 	}
@@ -94,12 +88,12 @@ func ReverseSlice(slice []int) []int {
 // FilterEven returns a new slice containing only the even numbers
 // from the original slice.
 func FilterEven(numbers []int) []int {
-	// Check for emty slice
+	// Check for empty slice
 	if len(numbers) == 0 {
 		return []int{}
 	}
 
-	// New slice to store even elemenes
+	// New slice to store even elements
 	res := make([]int, 0)
 
 	// Loop numbers and check for even
