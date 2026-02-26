f12b719b85ae736d012473edbadcc1813f7703fe
shansing <i@shansing.com>
2026-02-26 10:02:53 +0800

    remove unwanted text

--- challenge-6/submissions/shansing/solution-template.go
+++ challenge-6/submissions/shansing/solution-template.go
@@ -29,9 +29,6 @@ func CountWordFrequency(text string) map[string]int {
 		if normalized == "" {
 			continue
 		}
-3
-Add solution for Challenge 17 by shansing
-
 		countMap[normalized]++
 	}
 	return countMap
